472dffd4b125236e6839ea13dd2c67b460aed291
Chris Love <christopherlove68@gmail.com>
2024-02-04 14:59:11 -0800

    Local CI updates

--- build.sh
+++ build.sh
@@ -3,12 +3,13 @@
 function ShowUsage()
 {
     cat <<EOT
-$(basename $0) options
+$(basename "$0") options
     [--builddir=<builddir>]  - name of build directory
     [--cxx=<path/to/cxx>]    - path for CXX environment variable
     [--cc=<path/to/cc>]      - path for CC env variable
     [--cmake=<options>]      - option string to pass to CMake
     [--cover]                - generate unit test and code coverage reports
+    [--concourse]            - building in Concourse
 EOT
     return 0    
 }
@@ -18,6 +19,7 @@ PARAM_CC=
 PARAM_CXX=
 PARAM_CMAKE=
 PARAM_COVER=0
+PARAM_CONCOURSE=
 
 while test $# -gt 0; do
     param="$1"
@@ -35,19 +37,22 @@ while test $# -gt 0; do
 
     case $param in
     builddir=*)
-        BUILDDIR=$(echo $param|cut -f2 -d'=')
+        BUILDDIR=$(echo "$param"|cut -f2 -d'=')
         ;;
     cc=*)
-        PARAM_CC=$(echo $param|cut -f2 -d'=')
+        PARAM_CC=$(echo "$param"|cut -f2 -d'=')
         ;;
     cxx=*)
-        PARAM_CXX=$(echo $param|cut -f2 -d'=')
+        PARAM_CXX=$(echo "$param"|cut -f2 -d'=')
         ;;
     cmake=*)
-        PARAM_CMAKE=$(echo $param|cut -f2- -d'=')
+        PARAM_CMAKE=$(echo "$param"|cut -f2- -d'=')
         ;;
     cover)
         PARAM_COVER=1
+        ;;
+    concourse*)
+        PARAM_CONCOURSE=1
         ;;
     help|h|?|-?)
         ShowUsage
@@ -71,21 +76,24 @@ echo "PARAM_CXX=$PARAM_CXX"
 echo "PARAM_CMAKE=$PARAM_CMAKE"
 echo "PARAM_COVER=$PARAM_COVER"
 
-[ ! -d ./config-cpp-git ] && { echo "ERROR: repo not cloned!"; exit 1; }
+# If running in a Concourse pipeline then validate the repo was cloned
+if [ -n "$PARAM_CONCOURSE" ]; then
+    [ ! -d ./config-cpp-git ] && { echo "ERROR: repo not cloned!"; exit 1; }
 
-# Change to the base directory of the repo
-cd config-cpp-git
+    # Change to the base directory of the repo
+    cd config-cpp-git || exit
+fi
 
 # Create build directory and switch to it
-mkdir -p $BUILDDIR
-cd $BUILDDIR
+mkdir -p "$BUILDDIR"
+cd "$BUILDDIR" || exit
 
 # Configure via CMake
-if [ -n "$PARAM_CXX" -o -n "$PARAM_CC" ]; then
+if [ -n "$PARAM_CXX" ] || [ -n "$PARAM_CC" ]; then
     # Override CC and CXX
-    CC=$PARAM_CC CXX=$PARAM_CXX cmake $PARAM_CMAKE -DCMAKE_BUILD_TYPE=DEBUG -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=ON ..
+    CC=$PARAM_CC CXX=$PARAM_CXX cmake "$PARAM_CMAKE" -DCMAKE_BUILD_TYPE=DEBUG -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=ON ..
 else
-    cmake $PARAM_CMAKE -DBUILD_TESTS=ON ..
+    cmake "$PARAM_CMAKE" -DBUILD_TESTS=ON ..
 fi
 ret=$?
 [ $ret -ne 0 ] && exit $ret
@@ -106,7 +114,9 @@ if [ $PARAM_COVER -eq 1 ]; then
         echo "Unit Test Report: http://fir.love.io:3004/dev/config-cpp/unit/"
 
         # Create code coverage tarball
-        cd ConfigCppCoverage/ && tar zcf ../coverage.tar.gz . && cd -
+        cd ConfigCppCoverage/ || exit
+        tar zcf ../coverage.tar.gz . 
+        cd - || exit
         curl -F item=@coverage.tar.gz http://fir.love.io:3004/artstore/config-cpp/coverage
         echo "Code Coverage: http://fir.love.io:3004/dev/config-cpp/coverage/"
     fi
